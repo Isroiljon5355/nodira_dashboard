311e21c445d7f73c8e4498a5a0d30c4c69192b40
Isroiljon5355 <isroiljon5355@gmail.com>
2023-05-31 22:33:23 +0500

    add

---
+++ src/sections/customer/customers-table.js
@@ -0,0 +1,127 @@
+import { useState } from "react";
+import { Box, Card } from "@mui/material";
+import MaterialTable from "material-table";
+import PropTypes from "prop-types";
+import { Scrollbar } from "src/components/scrollbar";
+import { forwardRef } from "react";
+
+import AddBox from "@material-ui/icons/AddBox";
+import ArrowDownward from "@material-ui/icons/ArrowDownward";
+import Check from "@material-ui/icons/Check";
+import ChevronLeft from "@material-ui/icons/ChevronLeft";
+import ChevronRight from "@material-ui/icons/ChevronRight";
+import Clear from "@material-ui/icons/Clear";
+import DeleteOutline from "@material-ui/icons/DeleteOutline";
+import Edit from "@material-ui/icons/Edit";
+import FilterList from "@material-ui/icons/FilterList";
+import FirstPage from "@material-ui/icons/FirstPage";
+import LastPage from "@material-ui/icons/LastPage";
+import Remove from "@material-ui/icons/Remove";
+import SaveAlt from "@material-ui/icons/SaveAlt";
+import Search from "@material-ui/icons/Search";
+import ViewColumn from "@material-ui/icons/ViewColumn";
+
+const tableIcons = {
+  Add: forwardRef((props, ref) => <AddBox {...props} ref={ref} />),
+  Check: forwardRef((props, ref) => <Check {...props} ref={ref} />),
+  Clear: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+  Delete: forwardRef((props, ref) => <DeleteOutline {...props} ref={ref} />),
+  DetailPanel: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
+  Edit: forwardRef((props, ref) => <Edit {...props} ref={ref} />),
+  Export: forwardRef((props, ref) => <SaveAlt {...props} ref={ref} />),
+  Filter: forwardRef((props, ref) => <FilterList {...props} ref={ref} />),
+  FirstPage: forwardRef((props, ref) => <FirstPage {...props} ref={ref} />),
+  LastPage: forwardRef((props, ref) => <LastPage {...props} ref={ref} />),
+  NextPage: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
+  PreviousPage: forwardRef((props, ref) => <ChevronLeft {...props} ref={ref} />),
+  ResetSearch: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+  Search: forwardRef((props, ref) => <Search {...props} ref={ref} />),
+  SortArrow: forwardRef((props, ref) => <ArrowDownward {...props} ref={ref} />),
+  ThirdStateCheck: forwardRef((props, ref) => <Remove {...props} ref={ref} />),
+  ViewColumn: forwardRef((props, ref) => <ViewColumn {...props} ref={ref} />),
+};
+
+export const CustomersTable = (props) => {
+  const { items = [] } = props;
+  // const { useState } = React;
+
+  const [columns, setColumns] = useState([
+    { title: "Ism", field: "name" },
+    { title: "Familiya", field: "surname", initialEditValue: "initial edit value" },
+    { title: "Tug'ilgan yil", field: "birthYear", type: "numeric" },
+    {
+      title: "Tug'ilgan joyi",
+      field: "birthCity",
+      lookup: { 34: "İstanbul", 63: "Şanlıurfa" },
+    },
+  ]);
+
+  const [data, setData] = useState([
+    { name: "Mehmet", surname: "Baran", birthYear: 1987, birthCity: 63 },
+    { name: "Zerya Betül", surname: "Baran", birthYear: 2017, birthCity: 34 },
+  ]);
+
+  return (
+    <Card>
+      <Scrollbar>
+        <Box sx={{ minWidth: 800 }}>
+          <MaterialTable
+            title="Barcha hodimlar"
+            columns={columns}
+            data={data}
+            icons={tableIcons}
+            options={{
+              exportButton: true,
+            }}
+            editable={{
+              onRowAdd: (newData) =>
+                new Promise((resolve, reject) => {
+                  setTimeout(() => {
+                    setData([...data, newData]);
+
+                    resolve();
+                  }, 1000);
+                }),
+              onRowUpdate: (newData, oldData) =>
+                new Promise((resolve, reject) => {
+                  setTimeout(() => {
+                    const dataUpdate = [...data];
+                    const index = oldData.tableData.id;
+                    dataUpdate[index] = newData;
+                    setData([...dataUpdate]);
+
+                    resolve();
+                  }, 1000);
+                }),
+              onRowDelete: (oldData) =>
+                new Promise((resolve, reject) => {
+                  setTimeout(() => {
+                    const dataDelete = [...data];
+                    const index = oldData.tableData.id;
+                    dataDelete.splice(index, 1);
+                    setData([...dataDelete]);
+
+                    resolve();
+                  }, 1000);
+                }),
+            }}
+          />
+        </Box>
+      </Scrollbar>
+    </Card>
+  );
+};
+
+CustomersTable.propTypes = {
+  count: PropTypes.number,
+  items: PropTypes.array,
+  onDeselectAll: PropTypes.func,
+  onDeselectOne: PropTypes.func,
+  onPageChange: PropTypes.func,
+  onRowsPerPageChange: PropTypes.func,
+  onSelectAll: PropTypes.func,
+  onSelectOne: PropTypes.func,
+  page: PropTypes.number,
+  rowsPerPage: PropTypes.number,
+  selected: PropTypes.array,
+};
---
+++ src/sections/overview/overview-tasks-progress.js
@@ -0,0 +1,64 @@
+import PropTypes from "prop-types";
+import ListBulletIcon from "@heroicons/react/24/solid/ListBulletIcon";
+
+import {
+  Avatar,
+  Box,
+  Card,
+  CardContent,
+  LinearProgress,
+  Stack,
+  SvgIcon,
+  Typography,
+} from "@mui/material";
+
+export const OverviewTasksProgress = (props) => {
+  const { value, sx } = props;
+
+  return (
+    <Card sx={sx}>
+      <CardContent>
+        <Stack alignItems="flex-start" direction="row" justifyContent="space-between" spacing={3}>
+          <Stack spacing={1}>
+            <Typography color="text.secondary" gutterBottom variant="overline">
+              Bir yilik natija
+            </Typography>
+            <Typography variant="h4">{value}%</Typography>
+          </Stack>
+          <Avatar
+            sx={{
+              backgroundColor: "warning.main",
+              height: 56,
+              width: 56,
+            }}
+          >
+            <SvgIcon>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="w-6 h-6"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M3 13.125C3 12.504 3.504 12 4.125 12h2.25c.621 0 1.125.504 1.125 1.125v6.75C7.5 20.496 6.996 21 6.375 21h-2.25A1.125 1.125 0 013 19.875v-6.75zM9.75 8.625c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125v11.25c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 01-1.125-1.125V8.625zM16.5 4.125c0-.621.504-1.125 1.125-1.125h2.25C20.496 3 21 3.504 21 4.125v15.75c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 01-1.125-1.125V4.125z"
+                />
+              </svg>
+            </SvgIcon>
+          </Avatar>
+        </Stack>
+        <Box sx={{ mt: 3 }}>
+          <LinearProgress value={value} variant="determinate" />
+        </Box>
+      </CardContent>
+    </Card>
+  );
+};
+
+OverviewTasksProgress.propTypes = {
+  value: PropTypes.number.isRequired,
+  sx: PropTypes.object,
+};
